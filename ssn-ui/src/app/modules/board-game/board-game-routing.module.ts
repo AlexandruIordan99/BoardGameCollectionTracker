36850ef637d137ef37271baa071f3adb53930cd9
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-01-13 11:46:37 +0100

    Updated routing

--- ssn-ui/src/app/modules/board-game/board-game-routing.module.ts
+++ ssn-ui/src/app/modules/board-game/board-game-routing.module.ts
@@ -1,17 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainComponent} from './components/pages/main/main.component';
-import {BoardGameListComponent} from './components/pages/board-game-list/board-game-list.component';
+import {MainComponent} from './pages/main/main.component';
+import {BoardGameListComponent} from './pages/board-game-list/board-game-list.component';
+import {MyBoardGamesComponent} from './pages/my-board-games/my-board-games.component';
+import {ManageBoardGamesComponent} from './pages/manage-board-games/manage-board-games.component';
+import {authGuard} from '../../services/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    canActivate:[authGuard],
     children:[
       {
         path: '',
-        component: BoardGameListComponent
-      }
+        component: BoardGameListComponent,
+        canActivate:[authGuard]
+      },
+      {
+        path:'my-board-games',
+        component: MyBoardGamesComponent,
+        canActivate:[authGuard]
+      },
+      {
+        path: 'manage',
+        component: ManageBoardGamesComponent,
+        canActivate:[authGuard]
+      },
+      {
+        path: 'manage/ :boardGameId',
+        component: ManageBoardGamesComponent,
+        canActivate:[authGuard]
+      },
     ]
   }
 ];
