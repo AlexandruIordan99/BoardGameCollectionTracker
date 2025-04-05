c380a4f7620f14f56bc9b777615b91b43b6280d0
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-04-05 13:38:14 +0200

    Updated formatting, no changes to content or function.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-security")
-
 	implementation("org.postgresql:postgresql:42.7.2")
-
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     compileOnly("org.projectlombok:lombok")
