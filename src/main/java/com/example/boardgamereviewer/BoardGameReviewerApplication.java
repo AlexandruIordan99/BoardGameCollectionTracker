f0f424b6b41610c09943f608f8d6fc6e35963f74
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-06-12 14:18:12 +0200

    Added entity scanner

--- src/main/java/com/example/boardgamereviewer/BoardGameReviewerApplication.java
+++ src/main/java/com/example/boardgamereviewer/BoardGameReviewerApplication.java
@@ -5,6 +5,7 @@ import com.example.boardgamereviewer.role.RoleRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
+@EntityScan("com.example.boardgamereviewer")
 
 public class BoardGameReviewerApplication {
 
