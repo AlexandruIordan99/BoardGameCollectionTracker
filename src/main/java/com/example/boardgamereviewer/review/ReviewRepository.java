a0d25fcbe9361dd2d6c2910cf4bd471b05fed6d2
AlexandruIordan99 <alexandru.iordan77@gmail.com>
2025-06-12 17:21:36 +0200

    Added a few queries

--- src/main/java/com/example/boardgamereviewer/review/ReviewRepository.java
+++ src/main/java/com/example/boardgamereviewer/review/ReviewRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
@@ -14,5 +17,29 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
    """)
 
     Page<Review> findAllByBoardGameId(Integer boardGameId, Pageable pageable);
+
+    @Query("""
+            SELECT review
+            FROM Review review
+            WHERE review.user.id = :userId
+            ORDER BY review.createdDate DESC 
+   """)
+    Page<Review> findByUserId(@Param("userId") Integer userId, Pageable pageable);
+
+    @Query("""
+            SELECT review
+            FROM Review review
+            WHERE review.user.id = :userId
+            ORDER BY review.createdDate DESC
+   """)
+    List<Review> findReviewsByUserId(@Param("userId") Integer userId);
+
+
+    @Query("""
+            SELECT COUNT(review)
+            FROM Review review
+            WHERE review.user.id = :userId
+   """)
+    Long countByUserId(@Param("userId") Integer userId);
 
 }
